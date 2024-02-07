8aacca6a744045ed5a29f1b1ce97359e303cddc1
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2024-02-07 23:50:11 +0200

    fix

--- New/dist/configs/mongo.config.d.ts
+++ New/dist/configs/mongo.config.d.ts
@@ -1,3 +1,5 @@
-import { ConfigService } from '@nestjs/config';
-import { TypegooseModuleOptions } from 'nestjs-typegoose';
-export declare const getMongoConfig: (configService: ConfigService) => Promise<TypegooseModuleOptions>;
+import { ConfigService } from "@nestjs/config";
+import { TypegooseModuleOptions } from "nestjs-typegoose";
+export declare const getMongoConfig: (
+    configService: ConfigService
+) => Promise<TypegooseModuleOptions>;
