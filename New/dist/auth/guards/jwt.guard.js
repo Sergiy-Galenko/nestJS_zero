8eb7a0fb060150b4abd66abc86621105d1941ac4
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2024-02-12 20:34:52 +0200

    update

--- New/dist/auth/guards/jwt.guard.js
+++ New/dist/auth/guards/jwt.guard.js
@@ -2,7 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.JwtAuthGuard = void 0;
 const passport_1 = require("@nestjs/passport");
-class JwtAuthGuard extends passport_1.AuthGuard('jwt') {
-}
+class JwtAuthGuard extends passport_1.AuthGuard("jwt") {}
 exports.JwtAuthGuard = JwtAuthGuard;
 //# sourceMappingURL=jwt.guard.js.map
